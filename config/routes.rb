fca9f9f58c1f88b13c824e7f2bad47ade7da1e16
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-04-11 11:48:41 -0400

    include route to index for about

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,13 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  resources :about, only: [:index]
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
   end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
