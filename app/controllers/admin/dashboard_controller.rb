1c798696b00d842b0d39cf2d7471e5e9adfea55d
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-04-11 10:06:41 -0400

    pass total count for Product and Category to admin dashboard template

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD'], only: :show
 
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
