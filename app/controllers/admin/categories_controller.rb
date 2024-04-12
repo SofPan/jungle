45d821b9bf1559808d2b164b5088a65578a85de8
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-04-12 12:48:55 -0400

    create template for new category form

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,4 +2,8 @@ class Admin::CategoriesController < ApplicationController
   def index
     @categories = Category.all.order(created_at: :desc)
   end
+
+  def new
+    @category = Category.new
+  end
 end
