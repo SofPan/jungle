b2b16384e028d83abc3b344803fcf32c8219d337
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-04-13 16:22:09 -0400

    create routes for user registration and creation

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
+  resource :register, only: [:show]
+  resource :users, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
