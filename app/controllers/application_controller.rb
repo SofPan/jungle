918b3ac45e6d299255db852b1f54514772e0003f
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-04-14 22:43:32 -0400

    create current_user and authorization defs to save current logged in user object

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   private
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def authorize
+    redirect_to '/login' unless current_user
+  end
 
   def cart
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
@@ -29,4 +38,5 @@ class ApplicationController < ActionController::Base
     }
     cookies[:cart]
   end
+
 end
