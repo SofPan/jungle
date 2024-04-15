ef445d75b48caa032159ba15dd6399abfb82b3ab
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-04-14 22:36:36 -0400

    create login and logout routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      # Store login cookie
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+    # If user's login doesn't work, send them back to the login form.
+      redirect_to '/login'
+    end
   end
 
   def destroy
+    # Clear login cookie
+    session[:user_id] = nil
+    redirect_to '/login'
   end
 end
