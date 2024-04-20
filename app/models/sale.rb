0a54794569523a794f879c27f55668c53791819e
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-04-20 12:08:59 -0400

    conditionally render sale status

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,13 @@
 class Sale < ApplicationRecord
+  def finished?
+    ends_on < Date.current
+  end
+
+  def upcoming?
+    starts_on > Date.current
+  end
+
+  def active?
+    !upcoming? && !finished?
+  end
 end
